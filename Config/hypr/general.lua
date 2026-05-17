fdce929e588b69e05df1e811f189951d60253118
lein <leinworld@gmail.com>
2026-05-17 19:16:19 +0900

    add rounding border

--- Config/hypr/general.lua
+++ Config/hypr/general.lua
@@ -2,6 +2,11 @@ hl.config({
 	general = {
 		resize_on_border = true,
 		hover_icon_on_border = true,
-		extend_border_grab_area = 15
+		extend_border_grab_area = 15,
+		gaps_out = 5
+	},
+	decoration = {
+		rounding = 5
 	}
+
 })
