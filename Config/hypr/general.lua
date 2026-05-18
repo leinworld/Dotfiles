debb63cf072b427b6cb87c4185e18293c5c0a671
lein <leinworld@gmail.com>
2026-05-19 00:02:16 +0900

    Change border

--- Config/hypr/general.lua
+++ Config/hypr/general.lua
@@ -3,7 +3,8 @@ hl.config({
 		resize_on_border = true,
 		hover_icon_on_border = true,
 		extend_border_grab_area = 15,
-		gaps_out = 5
+		gaps_out = 5,
+		border_size = 3
 	},
 	decoration = {
 		rounding = 5
