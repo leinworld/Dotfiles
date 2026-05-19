7ca1c22d9340cf573300ee62d451f2f13315ca4e
lein <leinworld@gmail.com>
2026-05-20 02:06:02 +0900

    Change Color

--- Config/hypr/general.lua
+++ Config/hypr/general.lua
@@ -4,7 +4,11 @@ hl.config({
 		hover_icon_on_border = true,
 		extend_border_grab_area = 15,
 		gaps_out = 5,
-		border_size = 3
+		border_size = 3,
+		col = {
+			active_border = "rgb(e7935c)",
+			inactive_border = "rgb(441305)"
+		}
 	},
 	decoration = {
 		rounding = 5
