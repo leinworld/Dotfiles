c689e3ba91eb42fb926466551d5575cb603c0d7f
lein <leinworld@gmail.com>
2026-05-30 01:48:13 +0900

    Change wallpaper settings

--- Config/hypr/general.lua
+++ Config/hypr/general.lua
@@ -12,6 +12,11 @@ hl.config({
 	},
 	decoration = {
 		rounding = 5
-	}
+	},
+	misc = {
+		force_default_wallpaper = 0,
+		disable_hyprland_logo = true,
+		disable_splash_rendering = true
+	},
 
 })
